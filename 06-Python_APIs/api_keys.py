df9c5305a4731459978a8897c259b38103ee9068
PhotoSynthesist63 <andrearose6208@outlook.com>
2020-07-26 14:30:43 -0700

    update files

--- 06-Python_APIs/api_keys.py
+++
@@ -1,6 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key1 = "e0101e812e681f8b19ce0d1a6a1187b4"
-weather_api_key2 = "cf1eed179b60750d44301fc12c948aa9"
-
-# Google API Key
-g_key = "AIzaSyAX2Njv3suCY5HWh9VU8ocg-WDVmFpmCxc"
